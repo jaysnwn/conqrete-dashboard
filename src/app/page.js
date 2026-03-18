7afdff7c22f7973ddd58b0c6af167997b898e4b4
jaysnwn <jaysnwn5@gmail.com>
2026-03-19 00:34:53 +0530

    new page

---
+++ src/app/page.js
@@ -0,0 +1,6 @@
+import { redirect } from "next/navigation";
+
+export default function Home() {
+  // Instantly routes anyone visiting the root URL straight to the dashboard
+  redirect("/dashboard"); 
+}G
